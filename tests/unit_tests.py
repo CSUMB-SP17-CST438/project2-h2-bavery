db7c3697412faf1d60965a83729e4c886494a03e
Brandon <bavery@csumb.edu>
2017-03-07 06:28:17 +0000

    debugging automated tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,11 +4,11 @@ import chatbot
 class ChatBotResponseTest(unittest.TestCase):
     def test_about(self):
         r = chatbot.get_chatbot_response('!! about ')
-        self.assertEquals(r, 'This is a chat room to discuss random things in a spacey environment. ')
+        self.assertEquals(r, 'This is a chat room to discuss random things in a spacey environment.')
     
     def test_help(self):
         r = chatbot.get_chatbot_response('!! help ')
-        self.assertEquals(r, 'Available commands: <br>  !! about ->  descriptions of the chat room <br>  !! help ->  lists available chatbot commands <br>  !! add <num> <num> ->  add two integers together <br>  !! divide <num> <num> ->  divide two integers <br>  !! say <text> -> the chatbot repeats the text input by the user  <br>   ')
+        self.assertEquals(r, 'Available commands: <br>  !! about ->  descriptions of the chat room <br>  !! help ->  lists available chatbot commands <br>  !! add <num> <num> ->  add two integers together <br>  !! divide <num> <num> ->  divide two integers <br>  !! say <text> -> the chatbot repeats the text input by the user  <br>  ')
     
     def test_add(self):
         r = chatbot.get_chatbot_response('!! add 2 3 ')
